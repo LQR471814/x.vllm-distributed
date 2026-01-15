d1c47c12b5ece7d03a184e7cdab056aeb2dc0df9
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-01-15 15:10:06 -0800

    init

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        config.cudaSupport = true; # Critical: tells nixpkgs to prefer CUDA versions
+      };
+    in
+    {
+      devShells.${system}.default =
+        let
+          libs = with pkgs; [
+            cudaPackages.cudatoolkit
+            cudaPackages.cudnn
+            linuxKernel.packages.linux_6_18.nvidia_x11
+          ];
+        in
+        pkgs.mkShell {
+          name = "cuda-python-env";
+          packages =
+            (with pkgs; [
+              uv
+            ])
+            ++ libs;
+          # Nixpkgs now automates much of the LD_LIBRARY_PATH via 'autoAddDriverRunpath'
+          # but a shellHook is often still needed for user-installed pip packages.
+          shellHook = ''
+            export CUDA_PATH=${pkgs.cudaPackages.cudatoolkit}
+            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath libs}:$LD_LIBRARY_PATH"
+            echo "Devshell activated."
+          '';
+        };
+    };
+}
