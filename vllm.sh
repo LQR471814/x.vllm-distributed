753fe3a9bb1005ab2854065224b28ac950ca1c8a
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-01-15 18:36:09 -0800

    feat: update vllm

--- vllm.sh
+++ vllm.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export VLLM_HOST_IP="$(ip -j addr show | jq -r '.[ ] | .addr_info[].local | select(startswith("192.168.20"))')"
+export VLLM_DISTRIBUTED_EXECUTOR_CONFIG='{"placement_group_options":{"strategy":"SPREAD"}}'
 uv run vllm serve Qwen/Qwen3-0.6B \
 	--distributed-executor-backend ray \
 	--tensor-parallel-size 1 \
